a087d899f33f7ac310073352903a183048d2687a
Anne <aaodeh@mun.ca>
2020-08-24 17:05:51 -0230

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assests/scss/base.scss';
+import 'antd/dist/antd.css';
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
